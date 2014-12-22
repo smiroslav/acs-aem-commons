9ccb9edd831876e9f41ec53cf9ab4da9c3110d51
David Gonzalez <davidjgonzalez@gmail.com>
2014-12-22 18:23:23 -0500

    #395 - Http Request Logger; Config params

--- bundle/src/main/java/com/adobe/acs/commons/logging/impl/HttpRequestLogger.java
+++ bundle/src/main/java/com/adobe/acs/commons/logging/impl/HttpRequestLogger.java
@@ -20,14 +20,18 @@
 
 package com.adobe.acs.commons.logging.impl;
 
+import com.adobe.acs.commons.util.OsgiPropertyUtil;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
-import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,8 +45,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component(
         label = "ACS AEM Commons - HTTP Request Logger",
@@ -50,20 +60,6 @@ import java.util.Map;
         metatype = true,
         policy = ConfigurationPolicy.REQUIRE
 )
-@Properties({
-        @Property(
-                name = Constants.SERVICE_RANKING,
-                intValue = Integer.MIN_VALUE,
-                propertyPrivate = true
-        ),
-        @Property(
-                label = "Path pattern",
-                name = "pattern",
-                value = ".*",
-                propertyPrivate = false
-        )
-})
-@Service
 public class HttpRequestLogger implements Filter {
     private static final Logger log = LoggerFactory.getLogger(HttpRequestLogger.class);
 
@@ -76,6 +72,34 @@ public class HttpRequestLogger implements Filter {
             boolValue = DEFAULT_ENABLED)
     public static final String PROP_ENABLED = "enabled";
 
+
+    private static final String[] DEFAULT_ACCEPT_HEADERS = new String[]{};
+    private Map<String, Pattern> acceptHeaders = new HashMap<String, Pattern>();
+    @Property(label = "Accept Header patterns",
+            value = {})
+    public static final String PROP_ACCEPT_HEADERS = "accept.headers";
+
+
+    private static final String[] DEFAULT_ACCEPT_PATHS = new String[]{"/"};
+    private String[] acceptPaths = DEFAULT_ACCEPT_PATHS;
+    @Property(label = "Accept Header patterns",
+            value = {"/"})
+    public static final String PROP_ACCEPT_PATHS = "accept.paths";
+
+    private static final String[] DEFAULT_BLACKLIST_HEADERS = new String[]{"Authorization"};
+    private String[] blacklistHeaders = DEFAULT_BLACKLIST_HEADERS;
+    @Property(label = "Blacklist Headers",
+            value = {"Authorization"})
+    public static final String PROP_BLACKLIST_HEADERS = "blacklist.headers";
+
+    private static final String[] DEFAULT_BLACKLIST_PARAMETERS = new String[]{"j_password", "password"};
+    private String[] blacklistParameters = DEFAULT_BLACKLIST_PARAMETERS;
+    @Property(label = "Blacklist Params",
+            value = {"j_password", "password"})
+    public static final String PROP_BLACKLIST_PARAMETERS = "blacklist.parameters";
+
+    private ServiceRegistration filterRegistration;
+
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
 
@@ -86,27 +110,29 @@ public class HttpRequestLogger implements Filter {
                          final ServletResponse servletResponse,
                          final FilterChain filterChain) throws IOException, ServletException {
 
-        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if (servletRequest instanceof HttpServletRequest) {
+            final HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        if (accepts(request)) {
-            final StringWriter sw = new StringWriter();
-            final PrintWriter pw = new PrintWriter(sw);
+            if (accepts(request)) {
+                final StringWriter sw = new StringWriter();
+                final PrintWriter pw = new PrintWriter(sw);
 
-            pw.println();
+                pw.println();
 
-            pw.println(LINE);
+                pw.println(LINE);
 
-            pw.printf("[ %s ] %s", request.getMethod().toUpperCase(), request.getRequestURI());
-            pw.println();
-            pw.println(request.getRequestURL().toString());
+                pw.printf("[ %s ] %s", request.getMethod().toUpperCase(), request.getRequestURI());
+                pw.println();
+                pw.println(request.getRequestURL().toString());
 
-            printHeaders(request, pw);
-            printParameters(request, pw);
-            printMiscRequestInfo(request, pw);
+                printHeaders(request, pw);
+                printParameters(request, pw);
+                printMiscRequestInfo(request, pw);
 
-            pw.println(LINE);
+                pw.println(LINE);
 
-            log.trace(sw.toString());
+                log.trace(sw.toString());
+            }
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
@@ -190,7 +216,13 @@ public class HttpRequestLogger implements Filter {
 
             for (Enumeration<String> params = request.getParameterNames(); params.hasMoreElements();) {
                 final String param = params.nextElement();
-                pw.printf("%s: %s", param, request.getParameter(param));
+
+                if (ArrayUtils.contains(blacklistParameters, param)) {
+                    pw.printf("%s: %s", param, "**********");
+                } else {
+                    pw.printf("%s: %s", param, request.getParameter(param));
+                }
+
                 pw.println();
             }
         }
@@ -204,7 +236,12 @@ public class HttpRequestLogger implements Filter {
 
             for (Enumeration<String> headers = request.getHeaderNames(); headers.hasMoreElements();) {
                 final String header = headers.nextElement();
-                pw.printf("%s: %s", header, request.getHeader(header));
+
+                if (ArrayUtils.contains(blacklistHeaders, header)) {
+                    pw.printf("%s: %s", header, "**********");
+                } else {
+                    pw.printf("%s: %s", header, request.getHeader(header));
+                }
                 pw.println();
             }
         }
@@ -216,13 +253,103 @@ public class HttpRequestLogger implements Filter {
     }
 
     private boolean accepts(final HttpServletRequest request) {
-        return this.enabled && log.isTraceEnabled();
+        // Perform fastest checks first
+
+        // Enabled state
+        if (!this.enabled) {
+            return false;
+        }
+
+        // Paths
+        if (ArrayUtils.isNotEmpty(acceptPaths)
+                && !StringUtils.startsWithAny(request.getRequestURI(), acceptPaths)) {
+            return false;
+        }
+
+        // Headers
+        if (!acceptHeaders.isEmpty()) {
+            boolean foundMatchingHeader = false;
+            for (Map.Entry<String, Pattern> entry : acceptHeaders.entrySet()) {
+                String headerValue = request.getHeader(entry.getKey());
+                if (StringUtils.isNotBlank(headerValue)) {
+                    final Matcher matcher = entry.getValue().matcher(headerValue);
+                    if (matcher.matches()) {
+                        foundMatchingHeader = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!foundMatchingHeader) {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 
     @Activate
-    protected final void activate(final Map<String, String> config) {
+    protected final void activate(final ComponentContext ctx) {
+        Dictionary<?, ?> config = ctx.getProperties();
+
+        // Enabled
         enabled = PropertiesUtil.toBoolean(config.get(PROP_ENABLED), DEFAULT_ENABLED);
 
-        log.info("Enabled: {}", enabled);
+        // Accept Paths
+        acceptPaths = PropertiesUtil.toStringArray(config.get(PROP_ACCEPT_PATHS), DEFAULT_ACCEPT_PATHS);
+
+        // Accept Headers
+        Map<String, String> tmp =
+                OsgiPropertyUtil.toMap(PropertiesUtil.toStringArray(
+                        config.get(PROP_ACCEPT_HEADERS), DEFAULT_ACCEPT_HEADERS), "=");
+
+        for (Map.Entry<String, String> entry : tmp.entrySet()) {
+            final Pattern pattern = Pattern.compile(entry.getValue());
+            acceptHeaders.put(entry.getKey(), pattern);
+        }
+
+        if (enabled) {
+            Dictionary<String, String> filterProps = new Hashtable<String, String>();
+            filterProps.put(Constants.SERVICE_RANKING, String.valueOf(Integer.MIN_VALUE));
+            filterProps.put("pattern", ".*");
+            filterRegistration = ctx.getBundleContext().registerService(Filter.class.getName(), this, filterProps);
+        }
+
+        // Blacklist Headers
+        blacklistHeaders = PropertiesUtil.toStringArray(config.get(PROP_BLACKLIST_HEADERS),
+                DEFAULT_BLACKLIST_HEADERS);
+
+        // Blacklist Headers
+        blacklistParameters = PropertiesUtil.toStringArray(config.get(PROP_BLACKLIST_PARAMETERS),
+                DEFAULT_BLACKLIST_PARAMETERS);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+
+        pw.println();
+        pw.printf("Enabled: %s", enabled).println();
+        pw.printf("Path Prefixes: %s", Arrays.toString(acceptPaths)).println();
+        for (final Map.Entry<String, Pattern> entry : acceptHeaders.entrySet()) {
+            pw.printf("Accept Header ~> %s: %s", entry.getKey(), entry.getValue().pattern());
+        }
+
+        pw.printf("Blacklist Headers: %s", Arrays.toString(blacklistHeaders)).println();
+        pw.printf("Blacklist Parameters: %s", Arrays.toString(blacklistParameters)).println();
+
+        log.info(sw.toString());
+    }
+
+
+    @Deactivate
+    protected final void deactivate(final Map<String, String> config) {
+        if (filterRegistration != null) {
+            filterRegistration.unregister();
+            filterRegistration = null;
+        }
+
+        enabled = false;
+        acceptHeaders.clear();
+        acceptPaths = DEFAULT_ACCEPT_PATHS;
     }
 }
